4e3a9c44b3d3a5cc5e21d4e4222ff69fce7d55be
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-10-10 19:50:00 -0400

    created the button and the display

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import Dashboard from './components/Dashboard';
+import Display from './components/Display';
 
 function App() {
+  const [balls, setBalls] = useState(0);
+  const [strikes, setStrikes] = useState(0);
+  const [innings, setInnings] = useState(0);
+  const [errors, setErrors] = useState(0);
+  const [outs, setOuts] = useState(0);
+  const [hits, setHits] = useState(0);
+
   return (
     <div className="App">
-      <h1>test</h1>
+      <header data-testid="main-header" className="App-header">
+        <p> Scoreboard </p>
+        <Display balls={balls} strikes={strikes} errors={errors} innings={innings} outs={outs} hits={hits} />
+        <Dashboard balls={balls} strikes={strikes} setBalls={setBalls} setStrikes={setStrikes} innings={innings} errors={errors} outs={outs} setOuts={setOuts} setInnings={setInnings} setErrors={setErrors} hits={hits} setHits={setHits} />
+      </header>
     </div>
   );
 }
